cfac13ce54530234bac89fb3d67058911bfb1fa4
Moultree <okaykudes@gmail.com>
2023-03-04 16:24:13 +0600

    added findBy methods

--- src/main/java/com/example/kanban/repository/UserRepository.java
+++ src/main/java/com/example/kanban/repository/UserRepository.java
@@ -4,5 +4,11 @@ import com.example.kanban.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> { }
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByEmail(String email);
+    public Optional<User> findByUsername(String username);
+    public Optional<User> findByUsernameLikeIgnoreCase(String username);
+}
