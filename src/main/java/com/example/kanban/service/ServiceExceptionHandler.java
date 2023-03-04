8ad1c460a2dab12a6a926259551e9f14dac28d59
Tastyfruitloops <aleshka.lobanov.1703@gmail.com>
2023-03-04 18:50:47 +0500

    Added ExceptionHandler for services

--- src/main/java/com/example/kanban/service/ServiceExceptionHandler.java
+++ src/main/java/com/example/kanban/service/ServiceExceptionHandler.java
@@ -5,9 +5,11 @@ import com.example.kanban.exception.InvalidUserException;
 import com.example.kanban.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ControllerAdvice
 public class ServiceExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
