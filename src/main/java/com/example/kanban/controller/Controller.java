ae6daf489bb172afa7fad06394331bf7d9f947c9
Moultree <okaykudes@gmail.com>
2023-03-04 17:53:27 +0600

    removed unnecessary imports

--- src/main/java/com/example/kanban/controller/Controller.java
+++ src/main/java/com/example/kanban/controller/Controller.java
@@ -1,14 +1,15 @@
 package com.example.kanban.controller;
 
-import com.example.kanban.entity.Board;
-
-import java.lang.reflect.Type;
 import java.util.Optional;
 
 public interface Controller<T> {
-    public Iterable<T> getAll();
-    public Optional <T> getById(Long id);
-    public void add(T content);
-    public void update(Long id, T content);
-    public void delete(Long id);
+    Iterable<T> getAll();
+
+    Optional<T> getById(Long id);
+
+    void add(T content);
+
+    void update(Long id, T content);
+
+    void delete(Long id);
 }
