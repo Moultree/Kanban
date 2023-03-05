c7d2362557beed85c58367c91d1598cef63f2943
Moultree <okaykudes@gmail.com>
2023-03-05 11:58:53 +0600

    removed redundant Link files

--- src/main/java/com/example/kanban/repository/LinkRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.kanban.repository;
-
-import com.example.kanban.entity.Link;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LinkRepository extends JpaRepository<Link, Long> {
-}
