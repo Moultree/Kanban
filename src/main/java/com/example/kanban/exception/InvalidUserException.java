70626832b9e520270d9e8566d871817a7f417824
Moultree <okaykudes@gmail.com>
2023-03-05 12:01:39 +0600

    added fallback to super constructor

--- src/main/java/com/example/kanban/exception/InvalidUserException.java
+++ src/main/java/com/example/kanban/exception/InvalidUserException.java
@@ -1,7 +1,10 @@
 package com.example.kanban.exception;
 
 public class InvalidUserException extends Exception {
-    public InvalidUserException(String s) {
+    public InvalidUserException(String message) {
+        super(message);
+    }
 
+    public InvalidUserException() {
     }
 }
--- src/main/java/com/example/kanban/exception/NotFoundException.java
+++ src/main/java/com/example/kanban/exception/NotFoundException.java
@@ -1,11 +1,10 @@
 package com.example.kanban.exception;
 
 public class NotFoundException extends Exception {
-    public NotFoundException(String s) {
-
+    public NotFoundException(String message) {
+        super(message);
     }
 
     public NotFoundException() {
-
     }
 }
